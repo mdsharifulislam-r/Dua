6bb4f11a49a913eaf484f3d6c713e7d4b0c4bbff
MD Shariful Islam <masom3456@gmail.com>
2024-11-27 09:43:24 +0600

    error fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,8 @@ import Header from "@/components/Header/Header";
 import MainContainer, { Params } from "@/components/MainComponet/MainContainer";
 import Image from "next/image";
 
-export default async function Home({searchParams}:{searchParams:Params}) {
-  const paramsData = await searchParams
+export default async function Home({searchParams}:{searchParams:any}) {
+  const paramsData:Params = await searchParams
   return (
     <div className="w-full bg-gray-100 min-h-screen">
       <Header/>
